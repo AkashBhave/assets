c0124d1d46c0595277423e90c421bcb35c82f844
Akash Bhave <akashbbhave@gmail.com>
2021-02-06 10:37:56 -0500

    Load configuration into struct

--- main.go
+++ main.go
@@ -1,6 +1,41 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Assets []Asset `yaml:"assets"`
+}
+type Asset struct {
+	Name  string      `yaml:"name"`
+	Asset string      `yaml:"asset"`
+	Users []AssetUser `yaml:"users"`
+}
+type AssetUser struct {
+	Name string `yaml:"name"`
+	Key  string `yaml:"key"`
+}
+
+func config() *Config {
+	buffer, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatal("Please create a config.yaml")
+	}
+
+	config := &Config{}
+	err = yaml.Unmarshal(buffer, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
 
 func main() {
+	config := config()
+	fmt.Println(config)
 }
